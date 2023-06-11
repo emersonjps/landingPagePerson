e0b04708fafd628b8033537661244bb2ec77da6c
emersonjps-lia <emerson.pereira@sereducacional.com>
2023-06-11 18:08:56 -0300

    Adicionado novo layout para as infromações

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Emerson");
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+// ScrollTrigger.normalizeScroll(true);
+
+console.log("Emerson 2023");
+// gsap.to(["#skill", "#competence", "#footer_description"], {
+//   opacity: 0,
+//   duration: 0,
+// });
+gsap.from("#main", { opacity: 0, duration: 2 });
+
+// // create the smooth scroller FIRST!
+// let smoother = ScrollSmoother.create({
+//   smooth: 2,
+//   effects: true,
+// });
+
+// // pin box-c when it reaches the center of the viewport, for 300px
+// ScrollTrigger.create({
+//   trigger: ".box-c",
+//   pin: true,
+//   start: "center center",
+//   end: "+=300",
+//   markers: true,
+// });
