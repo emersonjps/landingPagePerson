7824a5a82093aac8a8f98cb330a3bdf28bdb48a5
emersonjps <emerson20010213@gmail.com>
2023-11-05 21:35:04 -0300

    new setup

---
+++ src/pages/components/NavComponent/NavComponent.tsx
@@ -0,0 +1,10 @@
+export default function NavComponent() {
+  return (
+    <nav className="fixed flex items-center justify-center gap-5 top-0 w-screen bg-[#20202380] p-5">
+      <button>Home</button>
+      <button>Competencias</button>
+      <button>Projetos</button>
+      <button>Sobre</button>
+    </nav>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,136 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import NavComponent from "./components/NavComponent/NavComponent";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <NavComponent />
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam
+        quos tenetur perferendis illum voluptate asperiores facilis praesentium.
+        Ratione, exercitationem dicta laudantium ad nesciunt ea rem autem
+        molestiae quasi provident.
+      </p>
+    </main>
+  );
+}
