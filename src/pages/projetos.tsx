8199ec41e692f166ce76e0854849a5b9c749f866
Emerson José Pereira da Silva <82915254+emersonjps@users.noreply.github.com>
2023-11-25 10:05:43 -0300

    Update projetos.tsx

--- src/pages/projetos.tsx
+++ src/pages/projetos.tsx
@@ -32,7 +32,7 @@ export default function Projetos() {
               widht={0}
               height={0}
               desctiption="Aplicação WEB, para gerar relatórios de compras de insumos por semestre. Surgiu da necessidade de facilitar compra dos insumos para a instituição de ensino superior, onde para cada aula prática presencial a um consumo considerável por equipes de insumos gerados, esta aplicação faz o cálculo com base na quantidade de alunos existente naquele período. Foi escolhido no front-end NEXTJS com Typescript para segurança de tipos na parte do cliente sendo potencializado pela gama de ferramentas de desenvolvimento do NEXTJS, no back-end foi escolhido a modelagem de um servidor STRAPI, por sua praticidade e versatilidade, integrado com AWS para armazenamento de mídia, e conectado ao banco de dados MYSQL na hostgator."
-              link="https://jogo-empresarial.herokuapp.com/"
+              link="https://relatorios-paps-ec4aa45d40b4.herokuapp.com/"
             />
             <CardProjeto
               img={p06}
